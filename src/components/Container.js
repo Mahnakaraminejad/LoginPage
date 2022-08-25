c501ec319f298f4eb35e154881b4945a30c07d35
sajadAD <sajadeydi8026@gmail.com>
2022-08-25 16:05:05 +0430

    test map & useeffects

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,25 +1,47 @@
-import "./Container.css"
-const Container =() =>{
-    return(
-        <div className="container">
-            <div className="coc">
-                <div className="inputs">
-                    <input type="email" id="email" placeholder="Enter your email address..." ></input> 
-                </div>
-                <div className="inputs">
-                    <input type="password" id="pass" placeholder="Enter your password... " ></input>
-                </div>
-                <div className="input-submit">
-                    <input type="submit" value="LOGIN" ></input>
-                    <a href=" ">Forgot password?</a>
-                    
-                </div>
-            </div>
-            
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./Container.css";
+const Container = () => {
+  const [rows, setRows] = useState([]);
 
-        </div>
-    );
-}
+  useEffect(() => {
+    axios
+      .get("https://my-json-server.typicode.com/typicode/demo/posts")
+      .then((response) => {
+        if (response.status === 200) {
+          setRows(response.data);
+        }else{
+            alert('sorry')
+        }
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="coc">
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>title</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <th scope="row">{item._id}</th>
+                  <td>{item.id}</td>
+                  <td>{item.title}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 //Enter your email address...
 //Enter your password...
